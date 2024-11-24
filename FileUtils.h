d142ff62d80d6897ffc184a11de9bde7fece7807
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-11-24 15:06:37 -0300

    fix: fixes format number

--- FileUtils.h
+++ FileUtils.h
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-struct CVRPResult {
+struct CVRPResult
+{
     string instanceName;
     int numClients;
     int vehicleCapacity;
@@ -22,18 +23,20 @@ struct CVRPResult {
     double RCL_ExecTime;
 };
 
-class FileUtils{
-    public: 
-
-    static CVRPInstance readInstanceFile(const string filePath) {
+class FileUtils
+{
+public:
+    static CVRPInstance readInstanceFile(const string filePath)
+    {
         ifstream inputFile(filePath);
         string aux, name;
         double comment, distance = -1, serviceTime = -1;
         int dimension, vehicleCapacity, depotIndex;
-        vector<int> demands;                 
+        vector<int> demands;
         vector<NodeCoord> coordinates;
 
-        if(!inputFile.is_open()) throw runtime_error("Erro ao abrir o arquivo!");
+        if (!inputFile.is_open())
+            throw runtime_error("Erro ao abrir o arquivo!");
 
         inputFile >> aux;
         inputFile >> aux;
@@ -44,7 +47,7 @@ class FileUtils{
         inputFile >> aux;
         inputFile >> comment;
         // cout << "instance comment: " << comment << "\n";
-        
+
         inputFile >> aux;
         inputFile >> aux;
         inputFile >> aux; // Ignore type
@@ -65,23 +68,26 @@ class FileUtils{
 
         inputFile >> aux; // NODE_COORD_SECTION or DISTANCE
 
-        if (aux == "DISTANCE") {
+        if (aux == "DISTANCE")
+        {
             inputFile >> aux;
             inputFile >> distance;
             inputFile >> aux;
         }
 
-        if (aux == "SERVICE_TIME") { // SERVICE_TIME or NODE_COORD_SECTION
+        if (aux == "SERVICE_TIME")
+        { // SERVICE_TIME or NODE_COORD_SECTION
             inputFile >> aux;
             inputFile >> serviceTime;
-            inputFile >> aux; 
-        } 
+            inputFile >> aux;
+        }
 
         // cout << distance << "\n";
 
         coordinates = vector<NodeCoord>(dimension + 1);
 
-        for(int i = 0; i < dimension; i++) {
+        for (int i = 0; i < dimension; i++)
+        {
             int nodeIndex;
             inputFile >> nodeIndex;
             inputFile >> coordinates[nodeIndex].x;
@@ -92,7 +98,8 @@ class FileUtils{
 
         demands = vector<int>(dimension + 1);
 
-        for(int i = 0; i < dimension; i++) {
+        for (int i = 0; i < dimension; i++)
+        {
             int nodeIndex;
             inputFile >> nodeIndex;
             inputFile >> demands[nodeIndex];
@@ -103,38 +110,39 @@ class FileUtils{
         inputFile >> depotIndex;
         // cout << "instance depot: " << depotIndex << "\n";
 
-        CVRPInstance instance = CVRPInstance(name, comment, dimension, vehicleCapacity, demands, coordinates, distance, serviceTime, depotIndex );
+        CVRPInstance instance = CVRPInstance(name, comment, dimension, vehicleCapacity, demands, coordinates, distance, serviceTime, depotIndex);
 
         inputFile.close();
 
         return instance;
-        
     }
 
-    static void saveCVRPResultsToCSV(const string& filePath, const vector<CVRPResult>& results) {
+    static string formatNumber(double value, int precision)
+    {
+        stringstream aux;
+        aux << fixed << setprecision(precision) << value;
+        string result = aux.str();
+        replace(result.begin(), result.end(), '.', ',');
+        return result;
+    }
+
+    static void saveCVRPResultsToCSV(const string &filePath, const vector<CVRPResult> &results)
+    {
         ofstream file(filePath);
 
         file << "Nome da Instância;Número de Clientes;Capacidade;Distância;Tempo de Serviço;CK WT;Tempo de Execucao CK_WT (s);RCL;Tempo de Execucao RCL (s)\n";
 
-        for (const auto& result : results) {
-            stringstream execTimeStream;
-            execTimeStream << fixed << setprecision(7) << result.CK_WT_ExecTime;  // Formata o tempo de execução com 7 casas decimais
-            string CK_WT_ExecTimeStr = execTimeStream.str();
-            replace(CK_WT_ExecTimeStr.begin(), CK_WT_ExecTimeStr.end(), '.', ',');  // Substitui o ponto decimal por vírgula (formato europeu)
-
-            execTimeStream << fixed << setprecision(7) << result.RCL_ExecTime;  // Formata o tempo de execução com 7 casas decimais
-            string RCL_ExecTimeStr = execTimeStream.str();
-            replace(RCL_ExecTimeStr.begin(), RCL_ExecTimeStr.end(), '.', ',');
-
+        for (const auto &result : results)
+        {
             file << result.instanceName << ";"
-                << result.numClients << ";"
-                << result.vehicleCapacity << ";"
-                << fixed << setprecision(2) << result.distance << ";"
-                << fixed << setprecision(2) << result.serviceTime << ";"
-                << fixed << setprecision(2) << result.CK_WT << ";"
-                << CK_WT_ExecTimeStr << ";"
-                << fixed << setprecision(2) << result.RCL << ";"
-                << RCL_ExecTimeStr << "\n";
+                 << result.numClients << ";"
+                 << result.vehicleCapacity << ";"
+                 << formatNumber(result.distance, 2) << ";"
+                 << formatNumber(result.serviceTime, 2) << ";"
+                 << formatNumber(result.CK_WT, 2) << ";"
+                 << formatNumber(result.CK_WT_ExecTime, 7) << ";"
+                 << formatNumber(result.RCL, 2) << ";"
+                 << formatNumber(result.RCL_ExecTime, 7) << "\n";
         }
 
         file.close();
